3b985dc1559fe5befc87dbd0af1cfcb9128d08c6
anjie_f <amfarmer2022@gmail.com>
2024-03-30 19:06:29 -0500

    Create change events

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ var subDurationElement = document.querySelector("#months");
 var result = document.querySelector(".result");
 var subType = "basic";
 var subDuration = 1;
+
+//CREATE change event listeners
+subTypeElement.addEventListener("change", function (e) {
+    subType = e.target.value;
+    console.log(subType);
+});
+
+subDurationElement.addEventListener("change", function (e) {
+    subDuration = Number(e.target.value);
+    console.log(subDuration);
+});
